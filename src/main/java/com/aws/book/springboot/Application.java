91c631dbb0f89338c132fd04f1e96fc2254977d5
김영민 <ymkim1085@funin.camp>
2023-01-29 17:45:01 +0900

    test: HelloController 테스트 코드 작성

--- src/main/java/com/aws/book/springboot/Application.java
+++ src/main/java/com/aws/book/springboot/Application.java
@@ -1,2 +1,12 @@
-package com.aws.book.springboot;public class Application {
+package com.aws.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
