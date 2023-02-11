3f3287d8ff9a6ab76628dbddb4c8c9e248cad530
김영민 <ymkim1085@funin.camp>
2023-02-11 20:18:15 +0900

    feat: 게시글 조회 API 추가
    * 메인 화면에 게시글 리스트 영역 추가
    * 게시글 전체 조회 레이어 추가

--- src/main/java/com/aws/book/springboot/web/IndexController.java
+++ src/main/java/com/aws/book/springboot/web/IndexController.java
@@ -1,21 +1,36 @@
 package com.aws.book.springboot.web;
 
+import com.aws.book.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+    /**
+     * 메인 화면 출력 API
+     * @param model
+     * @return
+     */
     @GetMapping("/")
-    public String index() {
-        log.info("Enter index page");
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
+    /**
+     * 글 등록 화면 출력 API
+     * @return
+     */
     @GetMapping("/posts/save")
     public String postsSave() {
-        return "posts-save"; // 글 등록 화면으로 이동
+        return "posts-save";
     }
 }
